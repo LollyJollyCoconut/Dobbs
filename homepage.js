fcef767bfa8783469e0d600cbdfe1e3f6051dc7e
LollyJollyCoconut <danielpeng929@gmail.com>
2023-10-16 06:00:32 -0700

    e
    e

--- homepage.js
+++ homepage.js
@@ -1,6 +1,14 @@
+let emojiFont;
+let pixelFont;
+let emojiList = ["🫠","🎃","🔥","❤️‍🔥","👻","⚡","🌈","🍓","🥥","🫥","💩","❤️","☠️","😈","🤡","💥","🦠","🧠","🫧","💧","❄️","🍔","🥞","🎲","🎨","🎄","🏈","⚽","🏀"];
+function preload() {
+	emojiFont = loadFont('NotoColorEmoji-Regular.ttf');
+	pixelFont = loadFont('PixelifySans-VariableFont_wght.ttf');
+}
 function setup() {
-
+	let homepageCanvas = createCanvas(windowWidth, windowHeight);
+	homepageCanvas.parent("#p5-canvas-homepage-div");
 }
 function draw() {
-	
+	background(0, 0, 0);
 }
