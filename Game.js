92947e9eeb360b087045b9b4e3c1f7cac21cd1c9
LollyJollyCoconut <danielpeng929@gmail.com>
2024-02-12 05:31:07 -0800

    h
    h

--- Game.js
+++ Game.js
@@ -1,6 +1,10 @@
 let easyButton = document.querySelector(".ez");
 let mediumButton = document.querySelector(".mid");
 let hardButton = document.querySelector(".sweat");
+let orderOfPlane = 6;
+let numOfSymbolsOnOneCard = orderOfPlane + 1;
+let cardDeck = [];
+let currentCard = [];
 easyButton.addEventListener("click", function(event){
 	localStorage.setItem("difficultyLevel","easy");
 	showEasyLevel();
@@ -34,6 +38,9 @@ function draw() {
 	background(255);
 }
 function showEasyLevel() {
+	orderOfPlane = 3;
+	buildCardDeck(orderOfPlane);
+	console.log(cardDeck);
 	mediumButton.classList.remove("btn-warning");
 	mediumButton.classList.add("btn-outline-warning");
 	hardButton.classList.remove("btn-warning");
@@ -42,6 +49,9 @@ function showEasyLevel() {
 	easyButton.classList.add("btn-warning");
 }
 function showMediumLevel() {
+	orderOfPlane = 6;
+	buildCardDeck(orderOfPlane);
+	console.log(cardDeck);
 	mediumButton.classList.remove("btn-outline-warning");
 	mediumButton.classList.add("btn-warning");
 	hardButton.classList.remove("btn-warning");
@@ -50,10 +60,40 @@ function showMediumLevel() {
 	easyButton.classList.add("btn-outline-warning");
 }
 function showHardLevel() {
+	orderOfPlane = 11;
+	buildCardDeck(orderOfPlane);
+	console.log(cardDeck);
 	mediumButton.classList.remove("btn-warning");
 	mediumButton.classList.add("btn-outline-warning");
 	hardButton.classList.remove("btn-outline-warning");
 	hardButton.classList.add("btn-warning");
 	easyButton.classList.remove("btn-warning");
 	easyButton.classList.add("btn-outline-warning");
+}
+function buildCardDeck(planeOrder) {
+	let numberOfSymbolsOnCard = planeOrder + 1;
+	cardDeck = [];
+	currentCard = [];
+	for(let i = 1; i <= planeOrder + 1; i++) {
+		currentCard.push(i);
+	}
+	cardDeck.push(currentCard);
+	for (let j = 1; j <= planeOrder; j++) {
+		currentCard = [];
+		currentCard.push(1);
+		for(let k = 1; k <= planeOrder; k++) {
+			currentCard.push(planeOrder*j + (k+1));
+		}
+		cardDeck.push(currentCard);
+	}
+	for (let i = 1; i <= planeOrder; i++) {
+		for (let j = 1; j <= planeOrder; j++) {
+			currentCard = [];
+			currentCard.push(i + 1);
+			for (let k = 1; k <= planeOrder; k++) {
+				currentCard.push(planeOrder+2+planeOrder*(k-1)+(((i-1)*(k-1)+j-1)%planeOrder));
+			}
+			cardDeck.push(currentCard);
+		}
+	}
 }
